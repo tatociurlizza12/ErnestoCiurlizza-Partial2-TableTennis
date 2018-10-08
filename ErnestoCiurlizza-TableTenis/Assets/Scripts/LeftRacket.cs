0b8fca01b50ae9fda47255ca1e5d3e199c890a74
Ernesto Ciurlizza <tciurlizzas@gmail.com>
2018-10-08 18:27:10 -0500

    Ping pongts

---
+++ ErnestoCiurlizza-TableTenis/Assets/Scripts/LeftRacket.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftRacket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.up * 0.3f);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.down * 0.3f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.back * 0.3f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.forward * 0.3f);
+        }
+
+	}
+}
---
+++ ErnestoCiurlizza-TableTenis/Assets/Scripts/RightRacket.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightRacket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.back * 0.3f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.forward* 0.3f);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.up * 0.3f);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.down * 0.3f);
+        }
+
+	}
+}
